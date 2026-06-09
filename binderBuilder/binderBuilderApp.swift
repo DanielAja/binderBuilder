684f04abd0ae3af16776f99b5ab81a9e7d68b56d
Daniel Aja <Danielaja@live.com>
2026-06-09 15:19:29 -0500

    Initial Commit

---
+++ binderBuilder/binderBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  binderBuilderApp.swift
+//  binderBuilder
+//
+//  Created by Daniel on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct binderBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
